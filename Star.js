142cda1bcda94cd7d42415419f37416625ce8ac0
Bailey Tincher <btincher99@gmail.com>
2017-11-21 22:18:57 -0600

    Playing with colors 3

--- Star.js
+++ Star.js
@@ -15,7 +15,7 @@ function Star() {
   }
 
   this.show = function() {
-    fill(random (1, 255));
+    fill(random (255));
     noStroke();
 
     var sx = map(this.x / this.z, 0, 1, 0, width);
@@ -29,7 +29,7 @@ function Star() {
 
     this.pz = this.z;
 
-    stroke(random (1, 255));
+    stroke(random (255));
     line(px, py, sx, sy);
 
   }
