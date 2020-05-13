f3127cdc504025533d32e016bdce1c1dcf551ce2
Bailey Tincher <btincher99@gmail.com>
2020-05-12 23:00:13 -0500

    Updates

--- styles-20d4938510b4a022b455.js
+++ styles-20d4938510b4a022b455.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[0],{268:function(n,w,o){}}]);
-//# sourceMappingURL=styles-98b3d9a13f97e85625a6.js.map
+//# sourceMappingURL=styles-20d4938510b4a022b455.js.map
--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9a456b59e6f163348613.js"
+    "url": "webpack-runtime-980f2a20745532c3d4a0.js"
   },
   {
     "url": "app-83429500ddd3bbe43ea6.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c864c9bd5fef33e3e7f220f90957d17d"
+    "revision": "d75ece1c515cfc5d85e446d4dba7c267"
   },
   {
     "url": "manifest.webmanifest",
